ff1628373423c85be71aab78ed0a5b554d6558b4
EdarArts <l.e.o.1996@hotmail.com>
2021-07-07 15:17:42 -0500

    New changes

---
+++ CrudTest/Context/CrudTest_DAL.cs
@@ -0,0 +1,140 @@
+﻿using CrudTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrudTest.Context
+{
+    public class CrudTest_DAL
+    {
+        string connectionString = "Data Source = DESKTOP-6VVSSAO ; Initial Catalog=CrudTestDB; Integrated Security=SSPI; User ID = edararts; Password=leo123;";
+        /// <summary>
+        /// Este metodo ayuda a obtener la lista de todos los users
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Users> GetAllUsers()
+        {
+            var usersList = new List<Users>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetAllUsers", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    var users = new Users();
+                    users.Id = Convert.ToInt32(dr["Id"].ToString());
+                    users.Nombre = dr["Nombre"].ToString();
+                    users.Apellido = dr["Apellido"].ToString();
+                    users.Email = dr["Email"].ToString();
+
+                    usersList.Add(users);
+                }
+                con.Close();
+            }
+            return usersList;
+        }
+        /// <summary>
+        /// Crear nuevo usuario
+        /// </summary>
+        /// <param name="users"></param>
+        public void CreateUsers(Users users)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_CreateNewUser", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Name", users.Nombre);
+                cmd.Parameters.AddWithValue("@Apellido", users.Apellido);
+                cmd.Parameters.AddWithValue("@Email", users.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Actualizar usuarios
+        /// </summary>
+        /// <param name="users"></param>
+        public void UpdateUsers(Users users)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_UpdateUsers", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Id", users.Id);
+                cmd.Parameters.AddWithValue("@Name", users.Nombre);
+                cmd.Parameters.AddWithValue("@Apellido", users.Apellido);
+                cmd.Parameters.AddWithValue("@Email", users.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Eliminar usuarios
+        /// </summary>
+        /// <param name="users"></param>
+        public void DeleteUsers(int? Id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_DeleteUsers", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Id", Id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Eliminar usuarios
+        /// </summary>
+        /// <param name="users"></param>
+        public Users GetUsersById(int? Id)
+        {
+            var users = new Users();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetUsersById", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Id", Id);
+                
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    users.Id = Convert.ToInt32(dr["Id"].ToString());
+                    users.Nombre = dr["Nombre"].ToString();
+                    users.Apellido = dr["Apellido"].ToString();
+                    users.Email = dr["Email"].ToString();
+                }
+                con.Close();
+            }
+            return users;
+        }
+    }
+}
---
+++ CrudTest/Controllers/UsersController.cs
@@ -0,0 +1,120 @@
+﻿using CrudTest.Context;
+using CrudTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudTest.Controllers
+{
+    public class UsersController : Controller
+    {
+        readonly CrudTest_DAL dbContext = new CrudTest_DAL();
+
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            List<Users> usersList = dbContext.GetAllUsers().ToList();
+            return View(usersList);
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id <=0) return NotFound();
+            Users users = dbContext.GetUsersById(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind] Users users)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    dbContext.CreateUsers(users);
+                    return RedirectToAction("Index");
+                }
+                return View(users);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0) return NotFound();
+            Users users = dbContext.GetUsersById(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind] Users users)
+        {
+            try
+            {
+                if (id <= 0) return NotFound();
+                if (ModelState.IsValid)
+                {
+                    dbContext.UpdateUsers(users);
+                    return RedirectToAction("Index");
+                }
+                return View(dbContext);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id <= 0) return NotFound();
+            Users users = dbContext.GetUsersById(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                dbContext.DeleteUsers(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
